b3b464cd1cad05694c90f7b748d880d93391795a
ahatuhov <ahatuhov@gmail.com>
2025-04-27 22:48:11 +0300

    Install Black and SQLAlchemy2_0

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from items_views import router as items_router
 from users.views import router as user_router
 
 app = FastAPI()
-app.include_router(items_router)  # регистрируем в (app) пространство имен в виде нового роутера
+app.include_router(
+    items_router
+)  # регистрируем в (app) пространство имен в виде нового роутера
 app.include_router(user_router)  # регистрируем в (app) пакет для users
 
 
@@ -17,11 +19,11 @@ def hello():
 
 # Запрос с query-параметрами
 @app.get("/hello/")
-def say_hello(name: str = "World"):  # для параметра 'name' значение по умолчанию равно "World"
+def say_hello(
+    name: str = "World",
+):  # для параметра 'name' значение по умолчанию равно "World"
     name = name.strip().title()
-    return {
-        "message": f"Hello {name}!"
-    }
+    return {"message": f"Hello {name}!"}
 
 
 @app.post("/calc/add/")
@@ -33,6 +35,6 @@ def add(a: int, b: int):
     }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # для автоматического перезапуска приложения
     uvicorn.run("main:app", reload=True)
